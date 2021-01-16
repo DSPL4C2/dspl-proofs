98ea3df999650ef22dd35d33bf3d8819810a7edb
Raposão <danilo-raposo@hotmail.com>
2021-01-16 14:01:38 -0300

    Product, Property and PC modeled as uninterpreted types

--- coq_general_theory/general_theory.v
+++ coq_general_theory/general_theory.v
@@ -1,43 +1,40 @@
-Inductive Product {X : Type} : Type :=
-  | Prod (x: X).
+Inductive Product : Type.
 
-Inductive Property {X : Type} : Type :=
-  | Proper (x: X).
+Inductive Property : Type.
 
-Inductive AlphaRelation {X Y : Type} : @Product X -> @Property Y -> (Product -> Property ) -> Prop :=
+Inductive AlphaRelation : Product -> Property -> (Product -> Property ) -> Prop :=
   | AR prod proper alpha (H : alpha prod = proper) : AlphaRelation prod proper alpha.
 
-Inductive PresenceCondition {X : Type} : Type :=
-  | PC (x : X).
+Inductive PresenceCondition : Type.
 
-Inductive ConfSatisfyPC {X : Type} : @PresenceCondition X -> (PresenceCondition -> bool) -> Prop :=
+Inductive ConfSatisfyPC : PresenceCondition -> (PresenceCondition -> bool) -> Prop :=
   | Satisfy pc conf (H: conf pc = true) : ConfSatisfyPC pc conf.
 
-Inductive VariableStructure (X Y : Type) : Type :=
+Inductive VariableStructure (X : Type) : Type :=
   | Base (x : X)
-  | Choice (pc : @PresenceCondition Y) (s1 s2 : VariableStructure X Y).
+  | Choice (pc : PresenceCondition) (s1 s2 : VariableStructure X).
 
-Definition AnnotativeModel {X Y : Type} : Type := VariableStructure (@Product X) Y.
+Definition AnnotativeModel : Type := VariableStructure Product.
 
-Definition AnnotativeExpression {X Y : Type} : Type := VariableStructure (@Property X) Y.
+Definition AnnotativeExpression : Type := VariableStructure Property.
 
-Inductive PiRelation {X Y : Type} : AnnotativeModel -> (PresenceCondition -> bool) -> Product -> Prop :=
-  | BaseCasePi (p : @Product X) (c: @PresenceCondition Y -> bool) : PiRelation (Base (Product) Y p) c p
-  | Choice1Pi (am1 am2 : AnnotativeModel) (p : @Product X) (pc : @PresenceCondition Y) (c : PresenceCondition -> bool) 
-      (H1 : c pc = true) (H2 : PiRelation am1 c p) : PiRelation (Choice (Product) Y pc am1 am2) c p
-  | Choice2Pi (am1 am2 : AnnotativeModel) (p : @Product X) (pc : @PresenceCondition Y) (c : PresenceCondition -> bool) 
-      (H1 : c pc = false) (H2 : PiRelation am2 c p) : PiRelation (Choice (Product) Y pc am1 am2) c p.
+Inductive PiRelation : AnnotativeModel -> (PresenceCondition -> bool) -> Product -> Prop :=
+  | BaseCasePi (p : Product) (c: PresenceCondition -> bool) : PiRelation (Base (Product) p) c p
+  | Choice1Pi (am1 am2 : AnnotativeModel) (p : Product) (pc : PresenceCondition) (c : PresenceCondition -> bool) 
+      (H1 : c pc = true) (H2 : PiRelation am1 c p) : PiRelation (Choice (Product) pc am1 am2) c p
+  | Choice2Pi (am1 am2 : AnnotativeModel) (p : Product) (pc : PresenceCondition ) (c : PresenceCondition -> bool) 
+      (H1 : c pc = false) (H2 : PiRelation am2 c p) : PiRelation (Choice (Product) pc am1 am2) c p.
 
-Inductive SigmaRelation {X Y : Type} : AnnotativeExpression -> (PresenceCondition -> bool) -> Property -> Prop :=
-  | BaseCaseSig (p : @Property X) (c: @PresenceCondition Y -> bool) : SigmaRelation (Base (Property) Y p) c p
-  | Choice1Sig (ae1 ae2 : AnnotativeExpression) (p : @Property X) (pc : @PresenceCondition Y) (c : PresenceCondition -> bool) 
-      (H1 : c pc = true) (H2 : SigmaRelation ae1 c p) : SigmaRelation (Choice (Property) Y pc ae1 ae2) c p
-  | Choice2Sig (ae1 ae2 : AnnotativeExpression) (p : @Property X) (pc : @PresenceCondition Y) (c : PresenceCondition -> bool) 
-      (H1 : c pc = false) (H2 : SigmaRelation ae2 c p) : SigmaRelation (Choice (Property) Y pc ae1 ae2) c p.
+Inductive SigmaRelation : AnnotativeExpression -> (PresenceCondition -> bool) -> Property -> Prop :=
+  | BaseCaseSig (p : Property) (c: PresenceCondition -> bool) : SigmaRelation (Base (Property) p) c p
+  | Choice1Sig (ae1 ae2 : AnnotativeExpression) (p : Property) (pc : PresenceCondition) (c : PresenceCondition -> bool) 
+      (H1 : c pc = true) (H2 : SigmaRelation ae1 c p) : SigmaRelation (Choice (Property) pc ae1 ae2) c p
+  | Choice2Sig (ae1 ae2 : AnnotativeExpression) (p : Property) (pc : PresenceCondition) (c : PresenceCondition -> bool) 
+      (H1 : c pc = false) (H2 : SigmaRelation ae2 c p) : SigmaRelation (Choice (Property) pc ae1 ae2) c p.
 
-Inductive HatAlphaRelation {X Y Z: Type} : AnnotativeModel -> AnnotativeExpression -> (Product -> Property) -> Prop :=
-  | BaseCase (prod : @Product X) (proper : @Property Y) (alpha : Product -> Property) 
-      (H : AlphaRelation prod proper alpha) : HatAlphaRelation (Base (Product) Z prod) (Base (Property) Z proper) alpha
+Inductive HatAlphaRelation : AnnotativeModel -> AnnotativeExpression -> (Product -> Property) -> Prop :=
+  | BaseCase (prod : Product) (proper : Property) (alpha : Product -> Property) 
+      (H : AlphaRelation prod proper alpha) : HatAlphaRelation (Base (Product) prod) (Base (Property) proper) alpha
   | ChoiceCase (am1 am2 : AnnotativeModel) (ae1 ae2 : AnnotativeExpression) (alpha : Product -> Property) 
-      (pc : @PresenceCondition Z) (H1: HatAlphaRelation am1 ae1 alpha) (H2: HatAlphaRelation am2 ae2 alpha) : 
-      HatAlphaRelation (Choice (Product) Z pc am1 am2) (Choice (Property) Z pc ae1 ae2) alpha.
+      (pc : PresenceCondition) (H1: HatAlphaRelation am1 ae1 alpha) (H2: HatAlphaRelation am2 ae2 alpha) : 
+      HatAlphaRelation (Choice (Product) pc am1 am2) (Choice (Property) pc ae1 ae2) alpha.
