b269ef7fc24ab872a0e6aa5a77e647b1357376c8
Raposão <danilo-raposo@hotmail.com>
2021-03-07 10:19:53 -0300

    bottom-left quadrant proved

--- coq_general_theory/general_theory_tc.v
+++ coq_general_theory/general_theory_tc.v
@@ -1,4 +1,5 @@
 Require Import Coq.Lists.List.
+Require Import Coq.Program.Basics.
 
 Definition variables : Type := list bool.
 
@@ -202,3 +203,191 @@ Inductive HatSigmaCompRelation {Proper PC : Type} `{Property Proper} `{PresenceC
       (H2 : lzip = zip ladd (ce.(dependents) n)) (H3 : length ladd = length (ce.(dependents) n))
       (H4 : forall x : (ADD Proper) * (PC * nat), In x lzip -> HatSigmaCompRelation ce (snd(snd x)) partialADDC (fst x)) :
       HatSigmaCompRelation ce n partialADDC (fold_left partialADDC ladd (HatSigma (ce.(E) n))).
+
+Theorem zipnilr {X Y : Type} : forall (l : list X), zip l (@ nil Y) = nil.
+Proof.
+  intros. destruct l;simpl;unfold zip; reflexivity.
+Qed.
+
+Theorem zipLength {X Y : Type} : forall (l1 : list X) (l2 : list Y) (l3 : list (X * Y)),
+  l3 = zip l1 l2 -> length l1 = length l2 -> length l1 = length l3.
+Proof.
+  induction l1;intros.
+  - rewrite H. reflexivity.
+  - destruct l2. discriminate H0. destruct l3. discriminate H.
+    inversion H. simpl. rewrite <- H3. apply IHl1 in H3.
+    rewrite H3. reflexivity. inversion H0. reflexivity.
+Qed.
+
+Theorem SigmaProperEquivalence: forall {Proper PC : Type} `{Property Proper} `{PresenceCondition PC},
+  forall (ae : AnnotativeExpression) (c : PC -> bool) (p1 p2 : Proper),
+  (SigmaRelation ae c p1 /\ SigmaRelation ae c p2) -> p1 = p2.
+Proof.
+  intros. destruct H1. induction ae.
+  - inversion H1. inversion H2. rewrite H5 in H8. auto.
+  - inversion H1;inversion H2.
+    + apply IHae1. auto. auto.
+    + rewrite H8 in H15. discriminate H15.
+    + rewrite H8 in H15. discriminate H15.
+    + apply IHae2. auto. auto.
+Qed.
+(* Acredito que seja possível provar, mas até o momento não consegui*)
+Axiom SigmaCompProperEquivalence: forall {Proper PC : Type} `{Property Proper} `{PresenceCondition PC},
+  forall (l : list (PC * nat)) (ce : CompositionalExpression) (ae : AnnotativeExpression) 
+  (c : PC -> bool) (partialEC : partialECT) (p1 p2 : Proper),
+  (SigmaCompRelation_aux ce ae l c partialEC p1 /\
+  SigmaCompRelation_aux ce ae l c partialEC p2) -> p1 = p2.
+(* Parte da prova do Teorema acima *)
+(*Proof.
+  intros l. induction l;intros;destruct H1.
+  - inversion H1. inversion H2. apply (SigmaProperEquivalence ae c).
+    split;auto.
+  - inversion H1;inversion H2.
+    + assert(E : proper2 = proper3).
+      * apply (IHl ce (E ce (snd a)) c partialEC).
+        split;auto.*)
+
+Theorem mapComposition : forall (X Y Z : Type) (l : list X) (f : Y -> Z) (g : X -> Y),
+  map f (map g l) = map (compose f g) l.
+Proof.
+  induction l.
+  - intros. reflexivity.
+  - intros. simpl. rewrite IHl. reflexivity.
+Qed.
+
+Theorem mapEquivalence : forall (X Y Z : Type) (l1 : list X) (l2 : list Y) (l3 : list (X * Y))
+  (f1 : X -> Z) (f2 : X * Y -> Z), l3 = zip l1 l2 -> length l3 = length l1 -> 
+  (forall x : X * Y, In x l3 -> f1 (fst x) = f2 x) -> map f1 l1 = map f2 l3.
+Proof.
+  intros X Y Z. induction l1;intros.
+  - rewrite H. reflexivity.
+  - destruct l2. rewrite zipnilr in H. rewrite H in H0. discriminate H0.
+    destruct l3. discriminate H0. simpl. simpl in H. inversion H.
+    inversion H0. rewrite <- H4. apply (IHl1 _ _ f1 f2) in H4.
+    + rewrite H4. assert (H': f1 (fst p) = f2 p). apply H1.
+      left. reflexivity. rewrite H3 in H'. simpl in H'. rewrite H'.
+      reflexivity.
+    + auto.
+    + intros. apply H1. right. auto.
+Qed.
+
+Theorem foldlce {Proper PC : Type} `{Property Proper} `{PresenceCondition PC}:
+  forall (ce : CompositionalExpression) (n : nat) (c : PC -> bool) (partialEC : partialECT)
+    (partialADDC : partialADDCT) (ladd : list (ADD Proper)) (ae : AnnotativeExpression)
+    (lzip : list ((ADD Proper) * (PC * nat))) (add : ADD Proper) , lzip = zip ladd (dependents ce n) ->
+    length ladd = length (dependents ce n) -> (forall x : ADD Proper * (PC * nat), In x lzip ->
+    HatSigmaCompRelation ce (snd (snd x)) partialADDC (fst x)) -> 
+    (forall x : ADD Proper * (PC * nat), In x lzip ->
+    SigmaCompRelation_aux ce (E ce (snd (snd x))) (dependents ce (snd (snd x))) c partialEC
+    (fst x (confToVariable c))) ->
+    (SigmaCompRelation_aux ce ae (dependents ce n) c
+    partialEC ((fold_left partialADDC ladd add)
+    (confToVariable c)) <-> SigmaCompRelation_aux ce 
+    (fold_left partialEC (map (fun (x : (ADD Proper) * (PC * nat)) => 
+    if (c (fst (snd x))) then (Base Proper PC ((fst x) (confToVariable c))) else (Base Proper PC emptyProperty))
+    lzip) ae) nil c partialEC (fold_left partialADDC ladd add
+    (confToVariable c))).
+Proof.
+  intros ce n. induction (dependents ce n).
+  - intros. split.
+    + intros. rewrite zipnilr in H1. rewrite H1. simpl. auto.
+    + intros. rewrite zipnilr in H1. rewrite H1 in H5. simpl in H5. auto.
+  - intros. destruct ladd. discriminate H2. split.
+    + intros. simpl. simpl in H5. inversion H5.
+      * simpl in H1. rewrite H1. simpl. rewrite H8.
+        assert (H' : In (a0,a) lzip). rewrite H1. simpl.
+        left. reflexivity. apply H4 in H'. simpl in H'.
+        assert (H'' : proper2 = a0 (confToVariable c)).
+        apply (SigmaCompProperEquivalence (dependents ce (snd a)) ce
+        (E ce (snd a)) c partialEC). split;auto. rewrite <- H''.
+        destruct lzip. discriminate H1. inversion H1. rewrite <- H18.
+        apply (IHl c partialEC partialADDC _
+        (partialEC ae (Base Proper PC proper2)) _
+        (partialADDC add a0)) in H18.
+        { destruct H18. apply H16. auto. }
+        { simpl in H2. inversion H2. reflexivity. }
+        { intros. apply H3. simpl. right. auto. }
+        { intros. apply H4. simpl. right. auto. }
+      * simpl in H1. rewrite H1. simpl. rewrite H10. destruct lzip.
+        discriminate H1. inversion H1. rewrite <- H17.
+        apply (IHl c partialEC partialADDC _ 
+        (partialEC ae (Base Proper PC emptyProperty)) _
+        (partialADDC add a0))in H17.
+        { apply H17. auto. }
+        { inversion H2. reflexivity. }
+        { intros. apply H3. right. auto. }
+        { intros. apply H4. right. auto. }
+    + intros. destruct lzip. discriminate H1. inversion H1.
+      rewrite H7 in H5. simpl. simpl in H5. destruct (c (fst a)) eqn:E.
+      * apply (TruePCCaseSig _ _ _ _ _ (a0 (confToVariable c))).
+        { auto. }
+        { assert (H'': In p (p::lzip)). left. reflexivity.
+          apply H4 in H''. rewrite H7 in H''. simpl in H''. auto. }
+        { apply (IHl c partialEC partialADDC _ 
+          (partialEC ae (Base Proper PC (a0 (confToVariable c))))
+          _ (partialADDC add a0)) in H8.
+          { apply H8. auto. }
+          { inversion H2. reflexivity. }
+          { intros. apply H3. right. auto. }
+          { intros. apply H4. right. auto. } }
+      * apply FalsePCCaseSig. auto. apply 
+        (IHl c partialEC partialADDC _ 
+        (partialEC ae (Base Proper PC emptyProperty))
+        _ (partialADDC add a0)) in H8.
+        { apply H8. auto. }
+        { inversion H2. reflexivity. }
+        { intros. apply H3. right. auto. }
+        { intros. apply H4. right. auto. }
+Qed.
+
+Theorem commutative_feature_family_feature_produc {Proper PC : Type} `{Property Proper} `{PresenceCondition PC} :
+  forall (ce : CompositionalExpression) (c : PC -> bool) (partialEC : partialECT) 
+    (partialADDC : partialADDCT) (add : ADD Proper), 
+    (forall (ae : AnnotativeExpression) (l : list AnnotativeExpression),
+    fold_left partialADDC (map HatSigma l) (HatSigma ae) = 
+    HatSigma(fold_left partialEC l ae)) -> (forall (x : ADD Proper * (PC * nat))
+    (listzip : list ((ADD Proper) * (PC * nat))),
+    In x listzip -> c (fst (snd x)) = false -> HatSigmaCompRelation ce (snd (snd x)) 
+    partialADDC (fst x) -> (fst x) (confToVariable c) = emptyProperty) -> 
+    (forall (v : variables) (a1 a2 : ADD Proper) (l1 l2 : list (ADD Proper)),
+    (a1 v = a2 v /\ map (fun a : (ADD Proper) => a v) l1 = map 
+    (fun a : (ADD Proper) => a v) l2 )-> (fold_left partialADDC l1 a1) v = 
+    (fold_left partialADDC l2 a2) v) ->
+    HatSigmaCompRelation ce ce.(top) partialADDC add -> 
+    SigmaCompRelation ce c (add (confToVariable c)) partialEC.
+Proof.
+  intros. unfold SigmaCompRelation. induction H4.
+  - rewrite H4. constructor. induction (E ce n). 
+    + constructor.
+    + unfold HatSigma. unfold ite. destruct (c pc) eqn:E';
+      constructor;auto;rewrite PCEquivalence;rewrite E';auto.
+  - assert (H': (SigmaCompRelation_aux ce (E ce n) (dependents ce n) c
+    partialEC ((fold_left partialADDC ladd (HatSigma (E ce n)))
+    (confToVariable c)) <-> SigmaCompRelation_aux ce 
+    (fold_left partialEC (map (fun (x : (ADD Proper) * (PC * nat)) => 
+    if (c (fst (snd x))) then (Base Proper PC ((fst x) (confToVariable c))) 
+    else (Base Proper PC emptyProperty)) lzip) (E ce n)) nil c partialEC 
+    (fold_left partialADDC ladd (HatSigma (E ce n)) (confToVariable c)))).
+    apply foldlce;auto. apply H'. constructor. assert (H'': SigmaRelation
+    (fold_left partialEC (map (fun x : ADD Proper * (PC * nat) =>
+    if c (fst (snd x)) then Base Proper PC (fst x (confToVariable c))
+    else Base Proper PC emptyProperty) lzip) (E ce n)) c ((HatSigma
+    (fold_left partialEC (map (fun x : ADD Proper * (PC * nat) =>
+    if c (fst (snd x)) then Base Proper PC (fst x (confToVariable c))
+    else Base Proper PC emptyProperty) lzip) (E ce n))) (confToVariable c))).
+    apply commutative_family_product_family. assert (H''' : (fold_left 
+    partialADDC ladd (HatSigma (E ce n))) (confToVariable c) =  HatSigma 
+    (fold_left partialEC
+    (map (fun x : ADD Proper * (PC * nat) => if c (fst (snd x)) then Base 
+    Proper PC (fst x (confToVariable c)) else Base Proper PC emptyProperty) lzip) 
+    (E ce n)) (confToVariable c)).
+    + rewrite <- H1. rewrite mapComposition. unfold compose. apply H3.
+      split. reflexivity. rewrite mapComposition. unfold compose.
+      apply (mapEquivalence _ _ _ _ (dependents ce n)).
+      * auto.
+      * symmetry. apply (zipLength _ (dependents ce n));auto.
+      * intros. destruct (c (fst (snd x))) eqn: E.
+        { simpl. unfold constant. reflexivity. }
+        { simpl. unfold constant. apply (H2 _ lzip);try(auto). }
+    + rewrite H'''. auto.
+Qed.
