5082c5b93be0fe474bb11b45a17d57c930521817
Raposão <danilo-raposo@hotmail.com>
2021-06-09 18:28:55 -0300

    well founded axioms added

--- coq_general_theory/all.v
+++ coq_general_theory/all.v
@@ -5,3 +5,14 @@ Fixpoint All {T : Type} (P : T -> Prop) (l : list T) : Prop :=
   | nil => True
   | h :: t => P h /\ All P t
   end.
+
+Theorem In_map_theorem {X Y : Type} : forall (l : list X) (f g : X -> Y),
+  (forall x : X, In x l -> f x = g x) -> map f l = map g l.
+Proof.
+  induction l.
+  - intros. reflexivity.
+  - intros. simpl. assert (H1 : In a (a::l)).
+    simpl. left. reflexivity. apply H in H1. rewrite H1.
+    assert (H2 : map f l = map g l). apply IHl. intros.
+    apply H. right. apply H0. rewrite H2. reflexivity.
+Qed.
--- coq_general_theory/evolution_aware_theory_v2.v
+++ coq_general_theory/evolution_aware_theory_v2.v
@@ -15,12 +15,11 @@ Inductive RDG {asset : Type} `{Asset asset} : Type :=
 
 Inductive Evolution : Type :=
   | ID
-  | Mensage.
-(*
+  | Message
   | PC
   | AddFeature
   | RemoveFeature
-  | SubsequentModelEvol.*)
+  | SubsequentModelEvol.
 
 Class Model (model : Type) {asset : Type} `{Asset asset} : Type :=
 {
@@ -28,8 +27,8 @@ Class Model (model : Type) {asset : Type} `{Asset asset} : Type :=
   evolutionRDG : RDG -> (RDG -> Evolution) -> RDG;
   phi : RDG -> ADD float;
   phiInd' : RDG -> list (ADD float) -> ADD float;
-  AddedModels : model -> (model -> Evolution) -> model;
-  RemovedModels : model -> (model -> Evolution) -> model
+  AddedRDG : RDG -> (RDG -> Evolution) -> RDG;
+  RemoveADDdeps : RDG -> (RDG -> Evolution) -> list (ADD float) -> list (ADD float);
 }.
 
 Definition deps {asset : Type} `{Asset asset} (rdg : RDG) : list RDG :=
@@ -38,7 +37,7 @@ Definition deps {asset : Type} `{Asset asset} (rdg : RDG) : list RDG :=
   | RDG_cons a deps => deps
   end.
 
-Fixpoint Phi'Aux {model asset :Type} `{Asset asset} `{Model model}
+Fixpoint Phi'Aux {model asset :Type} {H1 : Asset asset} {H2 : Model model}
   (rdg : RDG) (delta : RDG -> Evolution) : (ADD float) := 
   match rdg with
   | RDG_leaf a => phi (evolutionRDG rdg delta)
@@ -46,8 +45,55 @@ Fixpoint Phi'Aux {model asset :Type} `{Asset asset} `{Model model}
                        | ID => phi rdg
                        | Message => phiInd' (evolutionRDG rdg delta)
                           (map (fun (x : RDG) => Phi'Aux (x) delta) deps)
+                       | PC => phiInd' (evolutionRDG rdg delta)
+                          (map (fun (x : RDG) => Phi'Aux (x) delta) deps)
+                       | SubsequentModelEvol => phiInd' rdg
+                          (map (fun (x : RDG) => Phi'Aux (x) delta) deps)
+                       | AddFeature => phiInd' (evolutionRDG rdg delta)
+                          ((phi (AddedRDG rdg delta))::(map
+                          (fun (x : RDG) => Phi'Aux (x) delta) deps))
+                       (*O coq estava reclamando da recursão ao 
+                         aplicar uma função nos deps, então foi preciso
+                         ajustar a função RemoveADDdeps para mudar a lista de ADDs,
+                         e não a lista de RDG*)
+                       | RemoveFeature => phiInd' (evolutionRDG rdg delta)
+                          (RemoveADDdeps rdg delta 
+                          (map (fun (x : RDG) => Phi'Aux (x) delta) deps))
                        end
   end.
+(*Outra modelagem para a função Phi'. Nessa modelagem, o delta utilizado no
+  map dos casos Mensage,PC,Add e Remove é o delta que sempre retorna o ID, tornando
+  a nossa modelagem menos genérica mas ainda de acordo com os nossos casos de 
+  evolução. Essa mudança pode ser utilizada para auxiliar na prova de propriedades
+  envolvendo os dependentes da evolução de um RDG*)
+(*
+Fixpoint Phi'Aux' {model asset :Type} {H1 : Asset asset} {H2 : Model model}
+  (rdg : RDG) (delta : RDG -> Evolution) : (ADD float) :=
+  match rdg with
+  | RDG_leaf a => phi (evolutionRDG rdg delta)
+  | RDG_cons a deps => match (delta rdg) with
+                       | ID => phi rdg
+                       | Message => phiInd' (evolutionRDG rdg delta)
+                          (map (fun (x : RDG) => Phi'Aux (x) 
+                          (fun r : RDG => ID)) deps)
+                       | PC => phiInd' (evolutionRDG rdg delta)
+                          (map (fun (x : RDG) => Phi'Aux (x) 
+                          (fun r : RDG => ID)) deps)
+                       | SubsequentModelEvol => phiInd' rdg
+                          (map (fun (x : RDG) => Phi'Aux (x) delta) deps)
+                       | AddFeature => phiInd' (evolutionRDG rdg delta)
+                          ((phi (AddedRDG rdg delta))::(map
+                          (fun (x : RDG) => Phi'Aux (x) (fun r : RDG => ID)) deps))
+                       (*O coq estava reclamando da recursão ao 
+                         aplicar uma função nos deps, então foi preciso
+                         ajustar a função RemoveADDdeps para mudar a lista de ADDs,
+                         e não a lista de RDG*)
+                       | RemoveFeature => phiInd' (evolutionRDG rdg delta)
+                          (RemoveADDdeps rdg delta 
+                          (map (fun (x : RDG) => Phi'Aux (x) 
+                          (fun r : RDG => ID)) deps))
+                       end
+  end.*)
 
 Definition Phi'Fun {model asset :Type} `{Asset asset} `{Model model}
   (mod : model) (delta : RDG -> Evolution) : ADD float := 
@@ -61,54 +107,50 @@ Axiom phiInd'IDEvolution : forall (model asset: Type) `{Asset asset} `{Model mod
   forall (rdg : RDG) (delta : RDG -> Evolution), delta rdg = ID ->
   evolutionRDG rdg delta = rdg.
 
-(*Axiom depsRDGEvolution : forall (model asset: Type) `{Asset asset} `{Model model},
+Axiom isomorphismPhiEvolution : forall (model asset: Type) `{Asset asset} `{Model model},
   forall (rdg : RDG) (delta : RDG -> Evolution) , delta rdg <> AddFeature /\
   delta rdg <> RemoveFeature -> 
-  length (deps rdg) = length (deps (evolutionRDG rdg delta)).*)
+   map (fun r : RDG => phi (evolutionRDG r delta)) (deps rdg) = 
+   map phi (deps (evolutionRDG rdg delta)).
 
-Axiom depsLengthRDGEvolution : forall (model asset: Type) `{Asset asset} 
-  `{Model model}, forall (rdg : RDG) (delta : RDG -> Evolution), 
-  length (deps rdg) = length (deps (evolutionRDG rdg delta)).
+Axiom well_founded_In_rdg : forall (asset : Type) `{Asset asset},
+  well_founded (fun r1 r2 : RDG => In r1 (deps r2)).
 
-Definition Phi'AuxDelta {model asset: Type} {H1 : Asset asset} {H2 : Model model}
-  (delta : RDG -> Evolution) : RDG -> Prop :=
-  (fun (r : RDG) => @Phi'Aux model asset H1 asset H1 H2 r delta = 
-  phi (evolutionRDG r delta)).
-(*Tentativa de Replicar a hipótese NNode'_case para implementar uma nova
-indução para o tipo RDG, já que a indução disponibilizada pelo coq para nested
-types é fraca*) (* Tentativa baseada no capitulo 3.8 do livro do Chlipala*)
-(*
-Theorem RDG_case {model asset: Type} {H1 : Asset asset} {H2 : Model model} :
-  forall (a : asset) (lr : list RDG) (delta : RDG -> Evolution),
-  (All (Phi'AuxDelta delta) lr )-> 
-   (Phi'AuxDelta delta)(RDG_cons a lr).
+Theorem well_founded_phi_equivalence {model asset :Type} 
+  `{Asset asset} `{Model model} : forall delta : RDG -> Evolution,
+  (forall rx : RDG, (forall ry : RDG,
+  (fun r1 r2 : RDG => In r1 (deps r2)) ry rx -> 
+  (fun r : RDG => Phi'Aux r delta = phi (evolutionRDG r delta)) ry) ->
+  (fun r : RDG => Phi'Aux r delta = phi (evolutionRDG r delta)) rx) ->
+  forall r : RDG, (fun r : RDG => Phi'Aux r delta = phi (evolutionRDG r delta)) r.
 Proof.
-  intros. unfold Phi'AuxDelta in *. destruct lr.
-  - simpl. destruct (delta (RDG_cons a nil)) eqn:H'.
-    + apply (phiInd'IDEvolution model asset (RDG_cons a nil)) in H'.
-      rewrite H'. reflexivity.
-    + assert (H'' : phiInd' (evolutionRDG (RDG_cons a nil) delta) 
-      (map phi (deps (evolutionRDG (RDG_cons a nil) delta))) = 
-      phi (evolutionRDG (RDG_cons a nil) delta)).
-      * apply (phiInd'Equivalence model asset).
-      * rewrite <- H''.
-        assert (H''' : deps (evolutionRDG (RDG_cons a nil) delta) = nil).
-        { apply length_zero_iff_nil. 
-          assert (L : length (deps (RDG_cons a nil)) = 0). reflexivity.
-          rewrite <- L. symmetry. apply (depsLengthRDGEvolution model asset). }
-        rewrite H'''. reflexivity.
-  - simpl. destruct (delta (RDG_cons a (r::lr))) eqn:H'.
-    + apply (phiInd'IDEvolution model asset (RDG_cons a (r :: lr))) in H'.
-      rewrite H'. reflexivity.
-    +*)
+  intros delta. apply well_founded_ind. apply well_founded_In_rdg.
+Qed.
 
 Theorem Phi'EquivalenceAux {model asset :Type} `{Asset asset} `{Model model} :
   forall (rdg : RDG) (delta : RDG -> Evolution),
   Phi'Aux rdg delta = phi (evolutionRDG rdg delta).
 Proof.
-  intros. apply (well_founded_ind (RDG) (fun r1 r2 : RDG => In r1 (deps r2))
-  (fun r : RDG => Phi'Aux r delta = phi (evolutionRDG r delta)) ).
-
+  intros. apply well_founded_phi_equivalence. intros.
+  destruct rx. reflexivity. destruct (delta (RDG_cons ass deps0)) eqn:D.
+  - simpl. rewrite D. apply (phiInd'IDEvolution model asset) in D. rewrite D.
+    reflexivity.
+  - simpl. rewrite D. rewrite <- (phiInd'Equivalence _ asset).
+    assert (H' : (map (fun x : RDG => Phi'Aux x delta) deps0) =
+    (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta))) ->
+    phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
+    (map (fun x : RDG => Phi'Aux x delta) deps0) =
+    phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
+    (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta)))).
+    intros. rewrite H3. reflexivity. apply H'.
+    assert (H'' : map (fun r : RDG => phi (evolutionRDG r delta)) 
+    (deps (RDG_cons ass deps0)) = map phi 
+    (deps (evolutionRDG (RDG_cons ass deps0) delta))).
+    { apply (isomorphismPhiEvolution _ asset). rewrite D. split;
+    intros H''';discriminate H'''. }
+    rewrite <- H''. simpl. simpl in H2. apply In_map_theorem.
+    apply H2.
+  -
 
 Theorem Phi'Equivalence {model asset :Type} `{Asset asset} `{Model model} :
   forall (m : model) (delta : RDG -> Evolution), 
@@ -120,32 +162,6 @@ Proof.
   - 
 
 
-
-
-Proof.
-  intros. unfold Phi'Fun. remember (buildRDG m) as rdg. 
-  destruct rdg. simpl. induction deps0.
-  - destruct (delta (RDG_cons ass nil)) eqn:H'.
-    + apply (phiInd'IDEvolution model asset) in H'.
-      rewrite H'. reflexivity.
-    + simpl. assert (H'' : phiInd' (evolutionRDG (RDG_cons ass nil) delta) 
-      (map phi (deps (evolutionRDG (RDG_cons ass nil) delta))) = 
-      phi (evolutionRDG (RDG_cons ass nil) delta)).
-      * apply (phiInd'Equivalence model asset).
-      * rewrite <- H''. 
-        assert (H''' : deps (evolutionRDG (RDG_cons ass nil) delta) = nil).
-        { apply length_zero_iff_nil. 
-          assert (L : length (deps (RDG_cons ass nil)) = 0). reflexivity.
-          rewrite <- L. symmetry. apply (depsLengthRDGEvolution model asset). }
-        rewrite H'''. reflexivity.
-  -
-
-
-
-induction (delta (RDG_cons ass deps0)) eqn:H'.
-  - apply (phiInd'IDEvolution model asset) in H'. rewrite H'.
-    reflexivity.
-  -
 
 
 
