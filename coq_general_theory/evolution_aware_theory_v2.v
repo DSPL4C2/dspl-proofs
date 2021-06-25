dbd4f5e0ac9800f786f8e15f3980a4702ded82fd
Raposão <danilo-raposo@hotmail.com>
2021-06-25 14:50:17 -0300

    phi'equivalence refactored

--- coq_general_theory/evolution_aware_theory_v2.v
+++ coq_general_theory/evolution_aware_theory_v2.v
@@ -61,53 +61,29 @@ Fixpoint Phi'Aux {model asset :Type} {H1 : Asset asset} {H2 : Model model}
                           (map (fun (x : RDG) => Phi'Aux (x) delta) deps))
                        end
   end.
-(*Outra modelagem para a função Phi'. Nessa modelagem, o delta utilizado no
-  map dos casos Mensage,PC,Add e Remove é o delta que sempre retorna o ID, tornando
-  a nossa modelagem menos genérica mas ainda de acordo com os nossos casos de 
-  evolução. Essa mudança pode ser utilizada para auxiliar na prova de propriedades
-  envolvendo os dependentes da evolução de um RDG*)
-(*
-Fixpoint Phi'Aux' {model asset :Type} {H1 : Asset asset} {H2 : Model model}
-  (rdg : RDG) (delta : RDG -> Evolution) : (ADD float) :=
-  match rdg with
-  | RDG_leaf a => phi (evolutionRDG rdg delta)
-  | RDG_cons a deps => match (delta rdg) with
-                       | ID => phi rdg
-                       | Message => phiInd' (evolutionRDG rdg delta)
-                          (map (fun (x : RDG) => Phi'Aux (x) 
-                          (fun r : RDG => ID)) deps)
-                       | PC => phiInd' (evolutionRDG rdg delta)
-                          (map (fun (x : RDG) => Phi'Aux (x) 
-                          (fun r : RDG => ID)) deps)
-                       | SubsequentModelEvol => phiInd' rdg
-                          (map (fun (x : RDG) => Phi'Aux (x) delta) deps)
-                       | AddFeature => phiInd' (evolutionRDG rdg delta)
-                          ((phi (AddedRDG rdg delta))::(map
-                          (fun (x : RDG) => Phi'Aux (x) (fun r : RDG => ID)) deps))
-                       (*O coq estava reclamando da recursão ao 
-                         aplicar uma função nos deps, então foi preciso
-                         ajustar a função RemoveADDdeps para mudar a lista de ADDs,
-                         e não a lista de RDG*)
-                       | RemoveFeature => phiInd' (evolutionRDG rdg delta)
-                          (RemoveADDdeps rdg delta 
-                          (map (fun (x : RDG) => Phi'Aux (x) 
-                          (fun r : RDG => ID)) deps))
-                       end
-  end.*)
 
 Definition Phi'Fun {model asset :Type} `{Asset asset} `{Model model}
   (mod : model) (delta : RDG -> Evolution) : ADD float := 
   Phi'Aux (buildRDG mod) delta.
 
+(*Axioma que descreve o comportamento do phiInd' em relação ao phi.
+  Utilizado para os casos em que a evolução não é ID*)
+
 Axiom phiInd'Equivalence : forall (model asset: Type) `{Asset asset} `{Model model},
   forall (rdg : RDG),
   phiInd' rdg (map phi (deps rdg)) = phi rdg.
+
+(*Axioma que descreve o comportamento dos dependentes da evolução de um rdg
+  cujo caso de evolução é a adição de feature*)
 
 Axiom depsAddEvolution : forall (model asset: Type) `{Asset asset} `{Model model},
   forall (rdg : RDG) (delta : RDG -> Evolution), delta rdg = AddFeature -> 
   exists (r : RDG) (lr : list RDG), deps (evolutionRDG rdg delta) = r :: lr /\
   r = AddedRDG rdg delta /\
   lr = map (fun r : RDG => evolutionRDG r delta) (deps rdg).
+
+(*Axioma que descreve o comportamento da evolução em um rdg que possui caso de
+  evolução SubsequentModelEvol*)
 
 Axiom subsequentModelAxiom : forall (model asset: Type) `{Asset asset} 
   `{Model model}, forall (r : RDG) (delta : RDG -> Evolution)
@@ -122,11 +98,15 @@ Axiom RemoveFeatureAxiom : forall (model asset: Type) `{Asset asset}
   ADDdepsRmvCase r delta (map (fun rdg : RDG => phi (evolutionRDG rdg delta))
   (deps r)) = map phi (deps (evolutionRDG r delta)).
 
+(*Axioma que descreve o comportamento da evolução de um rdg que possui caso de
+  evolução ID*)
+
 Axiom phiInd'IDEvolution : forall (model asset: Type) `{Asset asset} `{Model model},
   forall (rdg : RDG) (delta : RDG -> Evolution), delta rdg = ID ->
   evolutionRDG rdg delta = rdg.
 
-(*Definir Exemplo e tentar reduzir*)
+(*Axioma que descreve a preservação da estrutura do RDG em modelos em que a evolução
+  não muda a sua estrutura*)
 
 Axiom commutativeDepsEvolution : forall (model asset: Type) `{Asset asset} `{Model model},
   forall (rdg : RDG) (delta : RDG -> Evolution) , delta rdg <> AddFeature /\
@@ -178,17 +158,22 @@ Theorem Phi'EquivalenceAux {model asset :Type} `{Asset asset} `{Model model} :
   Phi'Aux rdg delta = phi (evolutionRDG rdg delta).
 Proof.
   intros. apply well_founded_phi_equivalence. intros.
-  destruct rx. reflexivity. destruct (delta (RDG_cons ass deps0)) eqn:D.
-  - simpl. rewrite D. apply (phiInd'IDEvolution model asset) in D. rewrite D.
-    reflexivity.
-  - simpl. rewrite D. rewrite <- (phiInd'Equivalence _ asset).
-    assert (H' : (map (fun x : RDG => Phi'Aux x delta) deps0) =
-    (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta))) ->
-    phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
-    (map (fun x : RDG => Phi'Aux x delta) deps0) =
-    phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
-    (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta)))).
-    intros. rewrite H3. reflexivity. apply H'.
+  destruct rx. reflexivity.
+  (*adicionando uma hipótese que é utilizada em vários casos do
+  delta rdg*)
+  assert (H' : (map (fun x : RDG => Phi'Aux x delta) deps0) =
+  (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta))) ->
+  phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
+  (map (fun x : RDG => Phi'Aux x delta) deps0) =
+  phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
+  (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta)))).
+  { intros. rewrite H3. reflexivity. }
+  destruct (delta (RDG_cons ass deps0)) eqn:D;
+  simpl; rewrite D; try (apply (phiInd'IDEvolution model asset) in D; 
+  rewrite D;reflexivity); rewrite <- (phiInd'Equivalence _ asset
+  (evolutionRDG (RDG_cons ass deps0) delta)).
+  (*Message Case*)
+  - apply H'.
     assert (H'' : map (fun r : RDG => phi (evolutionRDG r delta)) 
     (deps (RDG_cons ass deps0)) = map phi 
     (deps (evolutionRDG (RDG_cons ass deps0) delta))).
@@ -196,14 +181,8 @@ Proof.
     intros H''';discriminate H'''. }
     rewrite <- H''. simpl. simpl in H2. apply In_map_theorem.
     apply H2.
-  - simpl. rewrite D. rewrite <- (phiInd'Equivalence _ asset).
-    assert (H' : (map (fun x : RDG => Phi'Aux x delta) deps0) =
-    (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta))) ->
-    phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
-    (map (fun x : RDG => Phi'Aux x delta) deps0) =
-    phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
-    (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta)))).
-    intros. rewrite H3. reflexivity. apply H'.
+  (*Presence Condition Case*)
+  - apply H'.
     assert (H'' : map (fun r : RDG => phi (evolutionRDG r delta)) 
     (deps (RDG_cons ass deps0)) = map phi 
     (deps (evolutionRDG (RDG_cons ass deps0) delta))).
@@ -211,38 +190,38 @@ Proof.
     intros H''';discriminate H'''. }
     rewrite <- H''. simpl. simpl in H2. apply In_map_theorem.
     apply H2.
-  - simpl. rewrite D. rewrite <- (phiInd'Equivalence _ asset
-    (evolutionRDG (RDG_cons ass deps0) delta)). assert (H' :
+  (*Add Feature Case*)
+  - assert (H'' :
     forall (r : RDG) (l1 l2 : list (ADD float)), l1 = l2 ->
     phiInd' r l1 = phiInd' r l2). intros. rewrite H3. reflexivity.
-    apply H'. apply (depsAddEvolution model asset) in D.
+    apply H''. apply (depsAddEvolution model asset) in D.
     destruct D. destruct H3. destruct H3. destruct H4. rewrite H3. simpl.
-    rewrite H4. assert (H'' : forall (l1 l2 : list (ADD float)) (r : ADD float),
+    rewrite H4. assert (H''' : forall (l1 l2 : list (ADD float)) (r : ADD float),
     l1 = l2 -> r::l1 = r::l2). intros. rewrite H6. reflexivity.
-    apply H''. rewrite H5. simpl. rewrite map_phi_evolution_theorem.
+    apply H'''. rewrite H5. simpl. rewrite map_phi_evolution_theorem.
     apply In_map_theorem. apply H2.
-  - simpl. rewrite D. rewrite <- (phiInd'Equivalence _ asset).
-    assert (Hsub : phiInd' (RDG_cons ass deps0) 
+  (*Subsequent Model Evolution Case*)
+  - assert (Hsub : phiInd' (RDG_cons ass deps0) 
     (map (fun x : RDG => Phi'Aux x delta) deps0) = phiInd' 
     (evolutionRDG (RDG_cons ass deps0) delta) 
     (map (fun x : RDG => Phi'Aux x delta) deps0)). 
     apply (subsequentModelAxiom _ asset). auto.
-    rewrite Hsub. assert (H' : (map (fun x : RDG => Phi'Aux x delta) deps0) =
+    rewrite Hsub. assert (H'' : (map (fun x : RDG => Phi'Aux x delta) deps0) =
     (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta))) ->
     phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
     (map (fun x : RDG => Phi'Aux x delta) deps0) =
     phiInd' (evolutionRDG (RDG_cons ass deps0) delta)
     (map phi (deps (evolutionRDG (RDG_cons ass deps0) delta)))).
-    intros. rewrite H3. reflexivity. apply H'.
-    assert (H'' : map (fun r : RDG => phi (evolutionRDG r delta)) 
+    intros. rewrite H3. reflexivity. apply H''.
+    assert (H''' : map (fun r : RDG => phi (evolutionRDG r delta)) 
     (deps (RDG_cons ass deps0)) = map phi 
     (deps (evolutionRDG (RDG_cons ass deps0) delta))).
     { apply (commutativePhiEvolution _ asset). rewrite D. split;
     intros H''';discriminate H'''. }
-    rewrite <- H''. simpl. simpl in H2. apply In_map_theorem.
+    rewrite <- H'''. simpl. simpl in H2. apply In_map_theorem.
     apply H2.
-  - simpl. rewrite D. rewrite <- (phiInd'Equivalence _ asset).
-    apply In_map_theorem in H2. simpl in H2. rewrite H2.
+  (*Remove Feature Case*)
+  - apply In_map_theorem in H2. simpl in H2. rewrite H2.
     apply (RemoveFeatureAxiom model asset) in D. rewrite <- D.
     reflexivity.
 Qed.
